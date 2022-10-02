257c8a70849af3a90a6fbccc0fb66d69cb3b3914
joaoSouza <76063225+joaoSouza2121@users.noreply.github.com>
2022-10-02 12:09:29 -0300

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as f:
+    page_description = f.read()
+
+with open("requirements.txt") as f:
+    requirements = f.read().splitlines()
+
+setup(
+    name="test_dio_ds",
+    version="0.0.1",
+    author="joao souza",
+    author_email="studantjps21@gmail.com",
+    description="Entrega de projeto descomplicando pacotes",
+    long_description=page_description,
+    long_description_content_type="text/markdown",
+    url="my_github_repository_project_link"
+    packages=find_packages(),
+    install_requires=requirements,
+    python_requires='>=3.8',
+)
